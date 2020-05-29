25e6526938d49c3f20dc9880f71e87e5dcdc0d57
carmen <grubes@gmail.com>
2020-05-29 13:18:27 +0200

    added if statement for different country calculations

--- car.js
+++ car.js
@@ -6,5 +6,28 @@ document.getElementById("calculate").addEventListener("click", getCalculation,fa
                 var country = document.getElementById("select").value;
                 var horsepower = Number(document.getElementById("horsepower").value);
 
-                document.getElementById("result").innerHTML = ( name + ", your insurance costs: " + age * horsepower + " Euros");
+                var usa = Math.round(horsepower * 100/ age +50);
+                var russia = Math.round(horsepower * 120 / age +100);
+                var brazil = Math.round(horsepower * 80 / age +80);
+                var other = Math.round(horsepower * 105/ age + 45);
+
+
+                
+                if (country == "USA"){
+                    document.getElementById("result").innerHTML = (name + ", your insurance costs: " + usa + " Euros");
+                }
+
+                else if (country == "Russia"){
+                    document.getElementById("result").innerHTML = (name + ", your insurance costs: " + russia + " Euros");
+                }
+
+                else if(country == "Brazil"){
+                    document.getElementById("result").innerHTML = (name + ", your insurance costs: " + brazil + " Euros")
+                }
+
+                else{
+                    document.getElementById("result").innerHTML = ( name + ", your insurance costs: " + other + " Euros");
+                }
+
+                
             }
